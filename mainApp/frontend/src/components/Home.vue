e5e177ed12bc9f7c03651486e65e05e8361ed145
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-22 13:14:28 +0000

    Reimported Homepage

--- mainApp/frontend/src/components/Home.vue
+++ mainApp/frontend/src/components/Home.vue
@@ -1,95 +1,99 @@
 <template >
 
-<div class="home">
-    <br/>
-<h1> Listings </h1>
-<div class="d-grid gap-2 col-6 mx-auto">
-    <button class="btn btn-primary" type="submit"> <router-link to="/newitem" class="nav-link">New Item</router-link></button>
-</div>
-<br/>
-<div class="row">
-  <div class="input-group" style="margin-bottom:1rem;">
-    <input type="search" class="form-control rounded" placeholder="Search" />
-    <button type="button" class="btn btn-outline-primary">Search</button>
+  <div class="home">
+      <br/>
+  <h1> Listings </h1>
+  <div class="d-grid gap-2 col-6 mx-auto">
+      <button class="btn btn-primary" type="submit"> <router-link to="/newitem" class="nav-link">New Item</router-link></button>
   </div>
-  
-<table class="table border rounded bg-light shadow">
-        <thead>
-          <tr>
-            <th scope="col">Item Title</th>
-            <th scope="col">Image</th>
-            <th scope="col">Start Price</th>
-            <th scope="col">Ending</th>
-          </tr>
-        </thead>
-        <tbody class="align-middle" v-for="auction in auctions" :key="auction.itemFinishDate" @dblclick="$data.auction = auction">
-          <tr style="background:#E1E1E1;" >
-              <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemTitle}} </td>
-              <td v-if="skippableDate(auction.itemFinishDate)"> <img :src="auction.itemPicture.substring(39)" width="100"/></td>
-              <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemStartPrice}} </td>
-              <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemFinishDate}}</td>
-              <td v-if="skippableDate(auction.itemFinishDate)"></td>
-              <td v-if="skippableDate(auction.itemFinishDate)">
-                <tr ><input type=number min="0"/></tr>
-                <tr><button type="button" class="btn btn-primary">Bid Amount</button></tr>
-              </td>
-              <td v-if="skippableDate(auction.itemFinishDate)">
-                <tr><textarea/></tr>
-                <tr><button type="button" class="btn btn-primary">Submit Question</button></tr>
-              </td>
+  <br/>
+  <div class="row">
+    <div class="input-group" style="margin-bottom:1rem;">
+      <input type="search" class="form-control rounded" placeholder="Search" id="search" v-on:input="searchItem()" />
+      <button type="button" class="btn btn-outline-primary">Search</button>
+    </div>
+  </div>
+
+  </div>
+
+  <div v-for="auction in searchedAuctions" :key="auction.itemFinishDate" @dblclick="$data.auction = auction">
+    <div class="row" v-if="skippableDate(auction.itemFinishDate)">
+      <div class="col-5" style="">
+        <div class="card">
+          <div class="row">
+            <div class="col-6">
+              <img :src="auction.itemPicture.substring(39)" class="" width="200" max-height="300" style="margin-bottom:1rem; margin-left:auto; margin-right: auto;"/>
+            </div>
+            <div class="col-6">
+              <h5 class="card-title">{{auction.itemTitle}}</h5>
+              <p class="card-text">{{auction.itemDescription}}</p>
+              <h4 class="card-text">£{{auction.itemStartPrice}}</h4>
+              <br/>
+              <h6 class="card-text">Ends: {{auction.itemFinishDate}}</h6>
+              <br/>
+              <input type=number min="0" class="bg-light border border-black text-dark card-text" style="margin-bottom: 0.5rem; font-size:large;"/>
+              <br/>
+              <button type="button" class="btn btn-primary">Bid Amount</button>
+            </div>
+          </div>
+        </div>
+        <br/>
+      </div>
+
+      
+
+      <br/>
+      <div class="container col-7">
+        <div class="card">
+          
+          <textarea class="bg-light border border-black text-dark form-control" placeholder="Write your question here..."/>
+          <button type="button" class="btn btn-primary" v-bind:id="'questionBox'+auction.questionId">Submit Question</button>
     
-            </tr>
-          
-          
-          <tr v-if="skippableDate(auction.itemFinishDate)"> 
-            <th></th> <!-- Leave Empty -->
-            <th scope="col">Questions</th>
-          </tr>
-          <tr v-if="skippableDate(auction.itemFinishDate)">
-            <th></th> <!-- Leave Empty -->
-            <th scope="col">User</th>
-            <th scope="col">Question</th>
-          </tr>
-          
-          <tr v-for="question in questions" :key="question.questionId">  
-            <td></td> <!-- Leave Empty -->
-            <td>{{question.userId}}</td> <!-- GET Userid from question -->
-            <td>{{question.questionText}}</td> <!-- GET question from question -->
+          <br/>
+          <button type="button" class="btn btn-secondary" data-bs-toggle="collapse" :data-bs-target="'#collapse'+ auction.id" aria-expanded="false" aria-controls="collapseQuestion">Show Q&A</button>
 
-            <tr>
-              <th></th> <!-- Leave Empty -->
-              <th></th> <!-- Leave Empty -->
-              <th scope="col">Answer</th>
+            <div class="collapse" :id="'collapse'+ auction.id" style="text-align:left" v-for="question in questions" :key="question.questionId">
+              <br/>
+                <div v-if="question.auctionId === auction.id">
+                  <h5 class="card-title">{{question.userId}}</h5>
+                  <p>
+                    {{question.questionText}}
+                  </p>
+                  <div v-for="answer in answers"> 
+                    <div v-show="answer.questionId === question.id" style="margin-left:5rem;">
+                      <h6 class="card-title">{{answer.userId}}</h6>
+                      <p>
+                        {{answer.answerText}}
+                      </p>
+                    </div>
+                  </div>
+
+                  <button type="button" class="btn btn-link" data-bs-toggle="collapse" :data-bs-target="'#collapseAnswer'+ question.id" aria-expanded="false" aria-controls="collapseAnswer">Reply</button>
+                  <div class="collapse" :id="'collapseAnswer'+ question.id" >
+                  <textarea class="bg-light border border-black text-dark form-control" placeholder="Write your answer here..."/>
+                  <button type="button" class="btn btn-primary" style="margin-left:85%; width:15%">Submit Answer</button>
+                  </div>
+                  
+                  <hr/>
+
+                </div>
               
-            </tr>
+            </div>
+            
 
-            <tr>
-              <td></td> <!-- Leave Empty -->
-              <td></td> <!-- Leave Empty -->
-              <td></td> <!-- Leave Empty -->
-              <td>
-                <tr><textarea/></tr>
-                <tr><button type="button" class="btn btn-primary">Submit Answer</button></tr>
-              </td>
-            </tr>
-
-          </tr>
-
-          
-          
-
-
-        </tbody>
-      </table>
-
-</div>
-
-</div>
+            
+        </div>
+      </div>
+      <br/>
+      <!-- Put a <hr/> tag here if you want to seperate each listing with a horizontal line -->
+    </div>
+    <br/>
+  </div>
 </template>
 
 <script>
 
- 
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
         
   export default{
     name: 'Home',
@@ -120,6 +124,8 @@
     },
     async created(){
        await this.getAuctions();
+       await this.getQuestions();
+       await this.getAnswers();
       },
       methods:{
 
@@ -127,7 +133,21 @@
         var response = await fetch("http://127.0.0.1:8000/api/auction/")
         let data = await response.json()
         this.auctions = data 
+        this.searchedAuctions = data
       },
+
+      async getQuestions(){
+        var response = await fetch("http://127.0.0.1:8000/api/question/")
+        let data = await response.json()
+        this.questions = data 
+      },
+
+      async getAnswers(){
+        var response = await fetch("http://127.0.0.1:8000/api/answer/")
+        let data = await response.json()
+        this.answers = data 
+      },
+      
 
       async addAuction(){
         await this.getAuctions();
@@ -165,7 +185,12 @@
             }else{
                 return false
             }
-        }
+        },
+
+        searchItem(){ 
+          var searchedAuction =document.getElementById('search').value; 
+          this.searchedAuctions = this.auctions.filter(auction => auction.itemTitle.toLowerCase().includes(searchedAuction) || auction.itemDescription.toLowerCase().includes(searchedAuction)); 
+        },
 
       }
 
